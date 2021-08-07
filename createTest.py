2f405d71adbe0b4cf590acfbc6769ad579ed9c13
Mahi Venkatesh <mahi.venkatesh@u-blox.com>
2021-08-07 11:37:53 +0300

    main python script to create the assessment

---
+++ createTest.py
@@ -0,0 +1,4 @@
+candidateFirstName = input("Enter your first name: ") #1. get first name of candidate 
+candidateLastName  = input("Enter your last name: ")  #1. get last name of candidate 
+candidateEmailId   = input("Enter your e-mail Id: ")  #1. get candidate's email Id
+
